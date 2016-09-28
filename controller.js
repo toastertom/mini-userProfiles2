8a74709ff4e4f3e439966ba16255760c63e0ce03
ToasterTom <tate.thomas5595@gmail.com>
2016-09-27 18:34:00 -0600

    First Commit

--- controller.js
+++ controller.js
@@ -1,7 +1,9 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
 
   $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
+    mainService.getUsers().then(function(response){
+      
+    })
   }
 
 
